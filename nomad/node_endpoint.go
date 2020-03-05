69b1dd62b2621a96e50d05fd28bc4d957ff03d6b
Michael Schurter <mschurter@hashicorp.com>
2020-03-04 20:53:26 -0800

    wip prevent transitioning state on updates
    Only transition on drains and lost nodes
    
    Lost nodes don't set PreviousAllocation so this doesn't work.

--- nomad/node_endpoint.go
+++ nomad/node_endpoint.go
@@ -1119,7 +1119,7 @@ func (n *Node) UpdateAlloc(args *structs.AllocUpdateRequest, reply *structs.Gene
 			if state.TaskHandle != nil {
 				ds = string(state.TaskHandle.DriverState)
 			}
-			n.logger.Info("-----> UpdateAlloc()", "alloc", alloc.ID, "task_name", task, "driver_state", ds)
+			n.logger.Info("-----> node.UpdateAlloc()", "alloc", alloc.ID, "task_name", task, "driver_state", ds)
 		}
 
 		// Add an evaluation if this is a failed alloc that is eligible for rescheduling
